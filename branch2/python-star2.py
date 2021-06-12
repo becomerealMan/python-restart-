0d46e9bf983d00e4c081b311a428b77e7a66d2c6
becomerealMan <dlehgus0910@naver.com>
2021-06-12 18:03:35 +0900

    파이썬별그리기2

---
+++ branch2/python-star2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 22:12:01 2021
+
+@author: idohyeon
+"""
+
+import turtle
+t=turtle.Turtle()
+t.shape('turtle')
+t.left(36)
+
+for i in range(5):
+    t.forward(300)
+    t.left(144)
